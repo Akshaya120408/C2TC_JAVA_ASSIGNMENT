8382e194138d54b802a1551df0cdde15d4c94e43
Akshaya12 <akshayanallasamy12@gmail.com>
2025-08-19 16:49:18 +0530

    Commit

--- JavaAssignment/src/Assignment1/utilities/EmployeeUtilities.java
+++ JavaAssignment/src/Assignment1/utilities/EmployeeUtilities.java
@@ -2,23 +2,11 @@ package com.akshaya.assignment.one.utilities;
 import com.akshaya.assignment.one.employees.*;
 
 public class EmployeeUtilities {
-
-    /**
-     * Prints the details of an employee.
-     * @param emp Employee object
-     */
-    public static void printEmployeeDetails(Employee emp) {
-        System.out.println(emp.toString());
+    public static void printEmployeeDetails(Employee e) {
+        System.out.println(e.toString());
     }
-
-    /**
-     * Increases an employee's salary by a given percentage.
-     * Demonstrates usage of access modifiers via public setter and getter.
-     * @param emp Employee object
-     * @param percentage Percentage to increase
-     */
-    public static void increaseSalary(Employee emp, double percentage) {
-        double newSalary = emp.getSalary() + (emp.getSalary() * percentage / 100);
-        emp.setSalary(newSalary);
+    public static void increaseSalary(Employee e, double percentage) {
+        double newSalary = e.getSalary() + (e.getSalary() * percentage / 100);
+        e.setSalary(newSalary);
     }
 }
